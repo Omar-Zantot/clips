0d9915db1e705f2207dc414c4271c5e00d97517f
Omar-Zantout <omar82199@gmail.com>
2024-02-02 03:59:39 +0200

    Looping through tabs

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
-  styleUrls: ['./tab.component.scss']
+  styleUrls: ['./tab.component.scss'],
 })
 export class TabComponent {
-
+  @Input() tabTitle = '';
 }
