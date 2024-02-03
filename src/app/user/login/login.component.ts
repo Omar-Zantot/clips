903dfbeb9f00a79ea8f68dd2bed2a71361ab6090
Omar-Zantout <omar82199@gmail.com>
2024-02-03 03:14:36 +0200

    TemplateForm end

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  cerdentials = {
+    email: '',
+    password: '',
+  };
 
+  login() {
+    console.log(this.cerdentials);
+  }
 }
