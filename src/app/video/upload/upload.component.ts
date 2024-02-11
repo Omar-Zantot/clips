b2f61b52752e3e6c8d19f7ac99677fa11cea192d
Omar-Zantout <omar82199@gmail.com>
2024-02-11 15:17:40 +0200

    multi-step form

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -8,11 +8,14 @@ import { Component } from '@angular/core';
 export class UploadComponent {
   isDragOver = false;
   file: File | null = null;
+  nextStep = false;
   storeFile(event: Event) {
     this.isDragOver = false;
     this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
 
     if (!this.file || this.file.type !== 'video/mp4') {
+      return;
     }
+    this.nextStep = true;
   }
 }
