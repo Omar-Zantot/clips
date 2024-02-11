f326d573e23530fd2e13b65fad5d32a706929142
Omar-Zantout <omar82199@gmail.com>
2024-02-11 14:55:07 +0200

    hover effect sol & handel files

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.scss']
+  styleUrls: ['./upload.component.scss'],
 })
 export class UploadComponent {
+  isDragOver = false;
+  file: File | null = null;
+  storeFile(event: Event) {
+    this.isDragOver = false;
+    this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
 
+    if (!this.file || this.file.type !== 'video/mp4') {
+    }
+  }
 }
