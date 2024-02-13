bbba7f6075441a82629e3ba5479a2096efef2c39
Omar-Zantout <omar82199@gmail.com>
2024-02-13 22:25:20 +0200

    uploading and handling errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UserModule } from './user/user.module';
-import { NavComponent } from './nav/nav.component';
-import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+
+import { UserModule } from './user/user.module';
+import { VideoModule } from './video/video.module';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 @NgModule({
   declarations: [
@@ -31,7 +34,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    VideoModule,
+    AngularFireStorageModule,
+
     AppRoutingModule,
   ],
   providers: [],
--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { last } from 'rxjs/operators';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-upload',
@@ -6,9 +10,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload.component.scss'],
 })
 export class UploadComponent {
-  isDragOver = false;
+  // & {----- ----- ----- Fields ----- ----- ------}
+
+  // file
   file: File | null = null;
+
+  // drop event
+  isDragOver = false;
+  // when uploading
   nextStep = false;
+
+  // form
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
+  uploadForm = new FormGroup({
+    title: this.title,
+  });
+
+  // alert
+  showAlert = false;
+  alertMsg = 'Pleas wait! Your clip is being uploaded.';
+  alertColor = 'blue';
+  // btn disabled
+  inSubmission = false;
+
+  // progress of upload
+  persentage = 0;
+  // toggle the precentage
+  showPrecentage = false;
+
+  // & {----- ----- ----- Constru ----- ----- ------}
+
+  constructor(private storage: AngularFireStorage) {}
+
+  // & {----- ----- ----- methods ----- ----- ------}
+
+  // get the file(video) title
   storeFile(event: Event) {
     this.isDragOver = false;
     this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
@@ -16,6 +55,46 @@ export class UploadComponent {
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
     }
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
     this.nextStep = true;
+  }
+
+  // publish the clip
+  uploadfile() {
+    // reset alert
+    this.showAlert = true;
+    this.alertMsg = 'Pleas wait! Your clip is being uploaded.';
+    this.alertColor = 'blue';
+    this.inSubmission = true; // disable the btn
+    this.showPrecentage = true;
+
+    // create unique file name using 3rd party library uuid
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+
+    /** better user experience (progress). */
+    const task = this.storage.upload(clipPath, this.file);
+
+    task.percentageChanges().subscribe((progress) => {
+      this.persentage = (progress as number) / 100;
+    });
+
+    task
+      .snapshotChanges()
+      .pipe(last())
+      .subscribe({
+        next: (snapshot) => {
+          this.alertColor = 'green';
+          this.alertMsg =
+            'Success! Your clip is now ready to share with the world.';
+          this.showPrecentage = false;
+        },
+        error: (error) => {
+          this.alertColor = 'red';
+          this.alertMsg = 'Upload failed! Please try again later';
+          this.inSubmission = false; // enable the btn
+          this.showPrecentage = false;
+        },
+      });
   }
 }
